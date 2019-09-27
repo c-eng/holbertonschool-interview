bcb415eb7d8256294e2c5fd210737cb9b7e4584b
c-eng <ceng@cca.edu>
2019-09-27 21:12:44 +0000

    New: Adding linked list palindrome checking

---
+++ 0x02-linked_list_palindrome/0-is_palindrome.c
@@ -0,0 +1,53 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * is_palindrome - Determines if a linked list is a palindrome
+ *
+ * @head: pointer to head of a list
+ *
+ * Return: 1 if palindrome, 0 otherwise.
+ */
+
+int is_palindrome(listint_t **head)
+{
+	int count = 0;
+	int index = 0;
+	listint_t *strider = NULL;
+	int *array = NULL;
+
+	if (!head)
+		return (0);
+	if (!*head)
+		return (1);
+	strider = *head;
+	while (strider)
+	{
+		strider = strider->next;
+		count++;
+	}
+	if (count == 1)
+		return (1);
+	array = malloc(sizeof(int) * count);
+	if (!array)
+		return (0);
+	strider = *head;
+	while (strider)
+	{
+		array[index] = strider->n;
+		strider = strider->next;
+		index++;
+	}
+	for (index = 0; index < (count / 2); index++)
+	{
+		printf("%i vs %i\n", array[index], array[(count - 1) - index]);
+		if (array[index] != array[(count - 1) - index])
+		{
+			free(array);
+			return (0);
+		}
+	}
+	free(array);
+	return (1);
+}
---
+++ 0x02-linked_list_palindrome/README.md
@@ -0,0 +1,12 @@
+# 0x02. Linked list palindrome
+### Description
+Identify if a linked list contains a palindrome.
+
+## Directory Contents
+
+|   ***File/Folder***    |  **Description**                       |
+|---------------|---------------------------------------|
+| `README.md` |  Read me file. |
+
+### Authors
+*Cameron Eng* - [Github](https://github.com/c_eng/) || [Twitter](https://twitter.com/c33Eng)
