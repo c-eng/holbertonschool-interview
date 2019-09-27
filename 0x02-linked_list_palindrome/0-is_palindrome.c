55fef0fcbbb0fbeddf8c04d0ed837bcfac403907
c-eng <ceng@cca.edu>
2019-09-27 22:07:03 +0000

    Fix: Reverting previous commit

--- 0x02-linked_list_palindrome/0-is_palindrome.c
+++ 0x02-linked_list_palindrome/0-is_palindrome.c
@@ -3,30 +3,6 @@
 #include "lists.h"
 
 /**
- * llist_index_value - Retrieves node at an index in a linked list
- *
- * @head: head of linked list
- * @index: index to retrieve node from
- *
- * Return: node at list index, or NULL otherwise.
- */
-
-listint_t *llist_index(listint_t *head, int index)
-{
-	int idx = 0;
-	listint_t *strider = head;
-
-	while (head && idx < index)
-	{
-		strider = strider->next;
-		idx++;
-	}
-	if (idx == index && strider)
-		return (strider);
-	return (NULL);
-}
-
-/**
  * is_palindrome - Determines if a linked list is a palindrome
  *
  * @head: pointer to head of a list
@@ -39,6 +15,7 @@ int is_palindrome(listint_t **head)
 	int count = 0;
 	int index = 0;
 	listint_t *strider = NULL;
+	int *array = NULL;
 
 	if (!head)
 		return (0);
@@ -52,13 +29,24 @@ int is_palindrome(listint_t **head)
 	}
 	if (count == 1)
 		return (1);
-	for (; index < (count / 2); index++)
+	array = malloc(sizeof(int) * count);
+	if (!array)
+		return (0);
+	strider = *head;
+	while (strider)
 	{
-		if (llist_index(*head, index)->n !=
-		    llist_index(*head, (count - 1) - index)->n)
+		array[index] = strider->n;
+		strider = strider->next;
+		index++;
+	}
+	for (index = 0; index < (count / 2); index++)
+	{
+		if (array[index] != array[(count - 1) - index])
 		{
+			free(array);
 			return (0);
 		}
 	}
+	free(array);
 	return (1);
 }
