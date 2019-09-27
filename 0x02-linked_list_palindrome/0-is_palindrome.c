b8f5dc394876e127f8c1114c542405ef1eaef988
c-eng <ceng@cca.edu>
2019-09-27 22:11:41 +0000

    Fix: Reverting previous commit

--- 0x02-linked_list_palindrome/0-is_palindrome.c
+++ 0x02-linked_list_palindrome/0-is_palindrome.c
@@ -15,7 +15,7 @@ int is_palindrome(listint_t **head)
 	int count = 0;
 	int index = 0;
 	listint_t *strider = NULL;
-	int array[1000];
+	int *array = NULL;
 
 	if (!head)
 		return (0);
@@ -29,6 +29,9 @@ int is_palindrome(listint_t **head)
 	}
 	if (count == 1)
 		return (1);
+	array = malloc(sizeof(int) * count);
+	if (!array)
+		return (0);
 	strider = *head;
 	while (strider)
 	{
@@ -40,8 +43,10 @@ int is_palindrome(listint_t **head)
 	{
 		if (array[index] != array[(count - 1) - index])
 		{
+			free(array);
 			return (0);
 		}
 	}
+	free(array);
 	return (1);
 }
