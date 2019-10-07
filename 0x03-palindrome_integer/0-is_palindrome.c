b528e8b60efa8661d804a34955300a6d58d80e11
c-eng <ceng@cca.edu>
2019-10-07 19:59:46 +0000

    Fix: refactor and fixing ULONG_MAX case

--- 0x03-palindrome_integer/0-is_palindrome.c
+++ 0x03-palindrome_integer/0-is_palindrome.c
@@ -1,49 +1,4 @@
-#include <stdio.h>
 #include "palindrome.h"
-
-/**
- * bam - Not pow, unsigned longs only
- *
- * @base: base number
- * @exp: exponent
- *
- * Return: base^exp
- */
-
-unsigned long bam(unsigned long base, unsigned long exp)
-{
-	unsigned long result = 1;
-	unsigned long i = 0;
-
-	for (; i < exp; i++)
-		result = result * base;
-	return (result);
-}
-
-/**
- * palindrome_deux - determines palindromeness of an integer
- *
- * @n: integer to determine
- * @c: digit count of integer
- *
- * Return: 1 if palindrome, 0 otherwise
- */
-
-int palindrome_deux(unsigned long n, unsigned long c)
-{
-	unsigned long i = 0;
-	unsigned long t = 10;
-
-	for (i = 0; i < c / 2; i++)
-	{
-		if ((n % bam(t, i + 1)) / bam(t, i) ==
-		    (n % bam(t, c - i)) / bam(t, c - (i + 1)))
-			continue;
-		else
-			return (0);
-	}
-	return (1);
-}
 
 /**
  * is_palindrome - determines palindromeness of an integer
@@ -55,15 +10,20 @@ int palindrome_deux(unsigned long n, unsigned long c)
 
 int is_palindrome(unsigned long n)
 {
-	unsigned long c = 1;
-	unsigned long t = 10;
-	int output;
+	unsigned long div = 10;
+	unsigned long t = 0;
+	unsigned long b = 0;
 
-	while (t <= n)
+	while (n / div >= 10)
+		div = div * 10;
+	while (n > 9)
 	{
-		t = t * 10;
-		c++;
+		t = (n / div);
+		b = (n % 10);
+		if (t != b)
+			return (0);
+		n = (n % div) / 10;
+		div = div / 100;
 	}
-	output = palindrome_deux(n, c);
-	return (output);
+	return (1);
 }
--- 0x03-palindrome_integer/palindrome.h
+++ 0x03-palindrome_integer/palindrome.h
@@ -1,8 +1,6 @@
 #ifndef PALINDROME_H
 #define PALINDROME_H
 
-unsigned long bam(unsigned long base, unsigned long exp);
-int palindrome_deux(unsigned long n, unsigned long c);
 int is_palindrome(unsigned long n);
 
 #endif /*PALINDROME_H*/
