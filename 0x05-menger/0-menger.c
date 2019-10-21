70370894e5e3bece2cbdf420fc2c0804159df458
c-eng <ceng@cca.edu>
2019-10-21 02:00:32 +0000

    Fix: Changing calculation approach

--- 0x05-menger/0-menger.c
+++ 0x05-menger/0-menger.c
@@ -1,57 +1,6 @@
 #include <stdio.h>
 #include <math.h>
-#include <stdlib.h>
 #include "menger.h"
-
-/**
- * m_print - menger print
- *
- * @x: x array
- * @y: y array
- * @level: array size
- *
- * Return: void
- */
-void m_print(int *x, int *y, int level)
-{
-	int i = 0;
-
-	for (; i < level; i++)
-	{
-		if (x[i] == 1 && y[i] == 1)
-		{
-			printf(" ");
-			return;
-		}
-	}
-	printf("#");
-}
-
-/**
- * m_tern_add - not ternary addition
- *
- * @array: array of not ternary values
- * @size: size of not ternary array
- *
- * Return: void
- */
-void m_tern_add(int *array, int size)
-{
-	int i = 0;
-
-	array[0]++;
-	for (; i < size; i++)
-	{
-		if (array[i] > 2)
-		{
-			if (i == size - 1)
-				return;
-			array[i + 1]++;
-			array[i] = 0;
-		}
-	}
-}
-
 
 /**
  * menger - 2d menger sponge print
@@ -62,31 +11,30 @@ void m_tern_add(int *array, int size)
  */
 void menger(int level)
 {
-	int *arrax, *array;
-	int size, i, j;
+	int size, x, y, dx, dy, f;
 
-	if (level < 0)
-		return;
-	arrax = malloc(sizeof(int) * level);
-	array = malloc(sizeof(int) * level);
-	if (!arrax || !array)
-		return;
-	for (i = 0; i < level; i++)
-	{
-		arrax[i] = 0;
-		array[i] = 0;
-	}
 	size = (int)pow(3, level);
-	for (j = 0; j < size; j++)
+	for (y = 0; y < size; y++)
 	{
-		for (i = 0; i < size; i++)
+		for (x = 0; x < size; x++)
 		{
-			m_print(arrax, array, level);
-			m_tern_add(arrax, level);
+			dx = x;
+			dy = y;
+			f = 0;
+			while (dx && dy)
+			{
+				if (dx % 3 == 1 && dy % 3 == 1)
+				{
+					printf(" ");
+					f = 1;
+					break;
+				}
+				dx = dx / 3;
+				dy = dy / 3;
+			}
+			if (!f)
+				printf("#");
 		}
 		printf("\n");
-		m_tern_add(array, level);
-		for (i = 0; i < level; i++)
-			arrax[i] = 0;
 	}
 }
