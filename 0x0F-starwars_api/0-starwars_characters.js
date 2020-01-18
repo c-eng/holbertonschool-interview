9bf21c4ea1cbada52d3aa555af2f1edf995f2415
c-eng <ceng@cca.edu>
2020-01-18 07:40:01 +0000

    Fix: Fixing for timeout

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,27 +1,30 @@
 #!/usr/bin/node
-
 if (process.argv.length !== 3) {
   console.log('Usage: ./0-starwars_characters.js <film number>');
 } else {
+  getCharactersInOrder(process.argv[2], 'https://swapi.co/api/people/');
+}
+
+function getCharactersInOrder (filmID, chURL) {
   const request = require('request');
-  let chURL = 'https://swapi.co/api/people/';
-  while (chURL !== null) {
-    request(chURL, function (error, response, body) {
-      if (error) {
-        console.error(error);
-        return;
-      }
+  request(chURL, function (error, response, body) {
+    if (error) {
+      console.error(error);
+    } else {
       const json = JSON.parse(body);
-      const characters = json.results;
       chURL = json.next;
+      const characters = json.results;
       characters.forEach(function (character) {
         const films = character.films;
         films.forEach(function (film) {
-          if (film.includes(process.argv[2])) {
+          if (film.includes(filmID)) {
             console.log(character.name);
           }
         });
       });
-    });
-  }
+    }
+    if (chURL !== null) {
+      getCharactersInOrder(filmID, chURL);
+    }
+  });
 }
