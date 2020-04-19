8f561a08f6f72c579a9739e1b0ed5cf6facd5fda
c-eng <ceng@cca.edu>
2020-04-19 18:32:11 +0000

    Fix: Updating header file

--- 0x1D-avl_trees/binary_trees.h
+++ 0x1D-avl_trees/binary_trees.h
@@ -18,7 +18,10 @@ struct binary_tree_s
 };
 
 typedef struct binary_tree_s binary_tree_t;
+typedef struct binary_tree_s avl_t;
 
+binary_tree_t *binary_tree_node(binary_tree_t *root, int n);
+void binary_tree_print(const binary_tree_t *tree);
 int binary_tree_is_avl(const binary_tree_t *tree);
 int bst_check(const binary_tree_t *root, int min, int max);
 int b_check(const binary_tree_t *root);
